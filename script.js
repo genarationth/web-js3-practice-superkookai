b90c5172fc060c53722847256e8efd99bb533b9f
superkookai <weerawut@live.com>
2023-03-13 16:20:19 +0700

    completed old style function declaration

---
+++ script.js
@@ -0,0 +1,26 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function inform(firstName,location,hobby){
+    console.log("Hi, my name is " + firstName + ", I live in " + location + " and enjoy "+hobby);
+}
+
+console.log(add(5,6));
+console.log(subtract(9,15));
+console.log(multiply(7,7));
+console.log(divide(97,7));
+
+inform("Kai","Bangkok","drinking wine");
